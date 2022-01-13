a186c695c44552ee19d5ab17a85f1d7d545d2d94
caesarhu <hupeishun@gmail.com>
2022-01-13 15:41:47 +0800

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to math-tools
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/caesarhu/math_tools.clj
@@ -0,0 +1,63 @@
+(ns caesarhu.math-tools
+  (:require [clojure.math.numeric-tower :refer :all]))
+
+(defn square?
+  "Is n a square number?"
+  [n]
+  (-> n exact-integer-sqrt last zero?))
+
+(defn digits
+  ([n base]
+   (if (zero? n) [0]
+       (loop [n (abs n) result []]
+         (let [q (quot n base)]
+           (if (zero? n) result (recur q (cons (- n (* q base)) result)))))))
+  ([n]
+   (digits n 10)))
+
+(defn digits->number
+  ([xs base]
+   (reduce (fn [acc x]
+             (+' (*' acc base) x))
+           0 xs))
+  ([xs]
+   (digits->number xs 10)))
+
+(defn power-mod
+  " b^e mod m (using Java which solves some cases the pure clojure method has to be modified to tackle--i.e. with large b & e and 
+    calculation simplications when gcd(b, m) == 1 and gcd(e, m) == 1) "
+  [b e m]
+  (.modPow (biginteger b) (biginteger e) (biginteger m)))
+
+(defn factorial
+  [n]
+  (cond
+    (neg? n) (throw (Exception. "factorial argument must be nonnegative."))
+    (< n 2) 1
+    :else (reduce *' (range 2 (inc n)))))
+
+(defn- -binomial
+  "n and k must be positive."
+  [n k]
+  (quot (factorial n)
+        (*' (factorial k) (factorial (- n k)))))
+
+(defn binomial
+  [n k]
+  (if (neg? n)
+    (let [b (-binomial (- k n 1) k)]
+      (if (odd? n) (- b) b))
+    (-binomial n k)))
+
+(defn gcd*
+  [& xs]
+  (reduce (fn [acc x]
+            (if (= acc 1) 1
+                (gcd acc x)))
+          xs))
+
+(defn lcm*
+  [& xs]
+  (reduce (fn [acc x]
+            (lcm acc x))
+          xs))
